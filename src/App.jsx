3607e36f50e90d829a130706922af87450b52f35
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-16 11:39:01 +0500

    Set up Project enviroment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { AiFillHome } from "react-icons/ai";
+import "./App.css";
+
+function App() {
+	return (
+		<h1 className="text-3xl font-bold underline">
+			<AiFillHome size={24} color="#333" />
+			This is H1
+		</h1>
+	);
+}
+
+export default App;
