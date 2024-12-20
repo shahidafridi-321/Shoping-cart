faaa1c7ac04f294a3e97f977969d5f164d6781e4
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-20 10:12:22 +0500

    Create Responsive nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,40 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import React, { useState } from "react";
+import { NavBarItem } from "./NavBarItem";
 
 export const Navbar = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
-	const buttonClassess =
-		"text-gray-100 font-bold text-sm px-2 py-1  hover:bg-gray-100 hover:text-gray-800 rounded-lg transition duration-300";
+	const navBarItemsClassess =
+		"text-gray-100 hover:border-b-2 border-gray-200 font-bold text-sm px-2 py-1  transition duration-300";
 
-	const ul = (
-		<>
-			<li>
-				<a className={buttonClassess} href="#">
-					Home
-				</a>
-			</li>
-			<li>
-				<a className={buttonClassess} href="#">
-					Shop
-				</a>
-			</li>
-			<li>
-				<a className={buttonClassess} href="#">
-					Cart
-				</a>
-			</li>
-		</>
-	);
+	const links = [
+		{
+			title: "Home",
+			link: "/",
+		},
+		{
+			title: "Shop",
+			link: "/shop",
+		},
+		{
+			title: "Cart",
+			link: "/cart",
+		},
+	];
 
 	return (
-		<nav className="bg-gray-800 text-white fixed w-full">
+		<nav className="bg-gray-700 text-white sticky top-0 left-0 w-full">
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 				<div className="flex items-center justify-between h-16">
 					<div className="flex flex-row w-full justify-between">
 						<div className="text-xl font-bold">ShopShop</div>
 						<div className="hidden md:block">
-							<ul className="flex ml-10 items-baseline space-x-2">{ul}</ul>
+							<NavBarItem
+								links={links}
+								classes="flex gap-5 ml-10 items-baseline space-x-2"
+								itemsStyles={navBarItemsClassess}
+							/>
 						</div>
 						<div className="md:hidden">
 							<button
@@ -48,9 +48,11 @@ export const Navbar = () => {
 				</div>
 			</div>
 			{isOpen && (
-				<ul className="flex flex-col gap-y-2 md:hidden px-4 sm:px-6 pb-2">
-					{ul}
-				</ul>
+				<NavBarItem
+					links={links}
+					classes="flex flex-col gap-y-2 md:hidden px-4 sm:px-6 pb-2 transition-all duration-500"
+					itemsStyles={navBarItemsClassess}
+				/>
 			)}
 		</nav>
 	);
