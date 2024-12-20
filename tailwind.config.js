e78353d8612c2002c5893a4eaea04950b50f968a
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-20 10:09:44 +0500

    Add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				dark: "#01116f",
+			},
+		},
 	},
 	plugins: [],
 };
