9c5f65e32338c96a9228c27a11dbd0fca5a85830
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-21 12:24:31 +0500

    Create BestSeller section copm that fetches data from an api and pass down to child component

---
+++ src/components/BestSellerSection.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import { BestSellerItems } from "./BestSellerItems";
+
+export const BestSellerSection = () => {
+	const [items, setItems] = useState([]);
+	useEffect(() => {
+		const getItems = async () => {
+			const response = await fetch("https://fakestoreapi.com/products?limit=4");
+			const items = await response.json();
+			setItems([].concat(items));
+		};
+		getItems();
+	}, []);
+
+	return (
+		<div className="bg-gray-300 mx-auto py-10 text-center">
+			<h2 className="text-2xl font-semibold">Bestsellers</h2>
+			<BestSellerItems items={items} setItems={setItems} />
+		</div>
+	);
+};
