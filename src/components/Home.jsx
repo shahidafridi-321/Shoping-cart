230978b35200bc0d221e4290f6056cae35287ee9
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-21 12:26:14 +0500

    Render BestSeller Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import { BestSellerSection } from "./BestSellerSection";
 import { Hero } from "./Hero";
 
 export const Home = () => {
 	return (
 		<div className="w-full mx-auto">
 			<Hero />
+			<BestSellerSection />
 		</div>
 	);
 };
