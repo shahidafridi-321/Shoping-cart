63276b8cef0dba1db780fd717cc9ac5da5587bd6
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-21 12:25:45 +0500

    Test

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
-export const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+export const Cart = ({ data = "" }) => {
+	return (
+		<div className="flex flex-col">
+			Cart
+		</div>
+	);
+};
