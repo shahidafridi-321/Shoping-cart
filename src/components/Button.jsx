20a438e79bc20bf4d3c85c040ff22ded719bb4a9
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-21 12:25:17 +0500

    Add eventHandler function and id parameters

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const Button = ({ text = "Button", classes = "" }) => {
+export const Button = ({
+	text = "Button",
+	classes = "",
+	onClick = () => {},
+	id = "",
+}) => {
 	return (
 		<button
 			className={`${classes}  px-6 py-2 font-bold  rounded-lg transition-all duration-300`}
+			onClick={onClick}
+			id={id}
 		>
 			{text}
 		</button>
