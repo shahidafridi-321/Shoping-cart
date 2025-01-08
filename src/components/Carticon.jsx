20fe16de4cb0db85491168a5023d5a149bf7a389
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-08 19:12:22 +0500

    Add functionality for Cartsicon value is update when another item is added to cart

--- src/components/Carticon.jsx
+++ src/components/Carticon.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BsCart } from "react-icons/bs";
 
-export const Carticon = ({ value = 0 }) => {
+export const Carticon = ({ cartItems }) => {
 	return (
 		<div className="relative">
 			<BsCart className="text-2xl " />
 			<p className="text-[14px] absolute top-[-8px] right-[-15px] py-0 px-1 rounded-2xl font-bold  bg-red-600 text-white ">
-				{value}
+				{cartItems.length}
 			</p>
 		</div>
 	);
