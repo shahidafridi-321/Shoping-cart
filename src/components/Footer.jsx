5dfd0b4469aa1b9e377edd223fafd2e33207e33c
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-08 17:57:46 +0500

    Create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+	return (
+		<div className="w-full  flex justify-center items-center ">
+			<p className="text-gray-200 font-bold">All right reserved </p>
+		</div>
+	);
+};
