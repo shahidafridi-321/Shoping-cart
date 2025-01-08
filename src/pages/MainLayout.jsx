f36a7c6f02a9bc493e4e71b990594fb29904dadf
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-08 17:58:05 +0500

    Render footer

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Navbar } from "../components/Navbar";
+import { Home } from "../components/Home";
+import { Footer } from "../components/Footer";
 
 export const MainLayout = () => {
-  return (
-    <div>MainLayout</div>
-  )
-}
+	return (
+		<div className="flex flex-col">
+			<header>
+				<Navbar />
+			</header>
+			<main className="w-full bg-indigo-300">
+				<Home />
+			</main>
+			<footer className="w-full bg-dark py-5">
+				<Footer />
+			</footer>
+		</div>
+	);
+};
