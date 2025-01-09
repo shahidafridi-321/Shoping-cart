90d2979c2ff47ee822a15157891d2adb12b1f45d
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-09 17:53:15 +0500

    Display items in cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useOutletContext } from "react-router-dom";
 
-export const Cart = ({ data = "" }) => {
+export const Cart = () => {
+	const { cartItemsList, setCartItemsList } = useOutletContext();
 	return (
-		<div className="flex flex-col">
-			Cart
+		<div className="grid grid-cols-1 md:grid-cols-3  h-screen">
+			<div className="md:col-span-2 bg-green-600">
+				{cartItemsList.map((item) => (
+					<div key={item.id}>
+						<p>{item.title}</p>
+						<p>{item.price}</p>
+						<p>{item.quantity}</p>
+						<p>{item.totalPrice}</p>
+					</div>
+				))}
+			</div>
+			<div className="bg-red-500"></div>
 		</div>
 	);
 };
