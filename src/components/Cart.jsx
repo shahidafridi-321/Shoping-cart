2eafe33964297751e962223cd709b073a2279f45
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-09 18:25:59 +0500

    Make grid container's order changing arrangement on different devices

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,8 +4,8 @@ import { useOutletContext } from "react-router-dom";
 export const Cart = () => {
 	const { cartItemsList, setCartItemsList } = useOutletContext();
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-3  h-screen">
-			<div className="md:col-span-2 bg-green-600">
+		<div className="grid grid-cols-1 md:grid-cols-3  h-screen ">
+			<div className="md:col-span-2 bg-green-600 order-1 md:order-[-1]">
 				{cartItemsList.map((item) => (
 					<div key={item.id}>
 						<p>{item.title}</p>
@@ -15,7 +15,7 @@ export const Cart = () => {
 					</div>
 				))}
 			</div>
-			<div className="bg-red-500"></div>
+			<div className="bg-red-500 "></div>
 		</div>
 	);
 };
