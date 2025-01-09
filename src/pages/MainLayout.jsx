b41a1cb5f2e2d338a8df92f57ba6d44a8209da65
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-09 18:14:13 +0500

    Add data fetching logic

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import { Outlet } from "react-router-dom";
 
 export const MainLayout = () => {
 	const [cartItemsList, setCartItemsList] = useState([]);
+	const [items, setItems] = useState([]);
+	useEffect(() => {
+		const getItems = async () => {
+			const response = await fetch("https://fakestoreapi.com/products?limit=4");
+			const items = await response.json();
+			setItems(items);
+		};
+		getItems();
+	}, []);
 	return (
 		<div className="flex flex-col">
 			<header>
 				<Navbar numberOfItemsInCart={cartItemsList.length} />
 			</header>
 			<main className="w-full bg-indigo-300">
-				<Outlet context={{ cartItemsList, setCartItemsList }} />
+				<Outlet context={{ items, cartItemsList, setCartItemsList }} />
 			</main>
 			<footer className="w-full bg-dark py-5">
 				<Footer />
