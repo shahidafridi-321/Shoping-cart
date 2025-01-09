47fec827db503c815b31eb63b069f3de1b36abf3
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-09 18:52:41 +0500

    Remove un-used prop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,16 +3,12 @@ import { Hero } from "./Hero";
 import { useOutletContext } from "react-router-dom";
 
 export const Home = () => {
-	const { items, cartItemsList, setCartItemsList } = useOutletContext();
+	const { items, setCartItemsList } = useOutletContext();
 
 	return (
 		<div className="w-full mx-auto">
 			<Hero />
-			<BestSellerSection
-				cartItemsList={cartItemsList}
-				setCartItemsList={setCartItemsList}
-				data={items}
-			/>
+			<BestSellerSection setCartItemsList={setCartItemsList} data={items} />
 		</div>
 	);
 };
