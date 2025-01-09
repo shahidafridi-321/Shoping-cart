565982917b445660e15151f8e6fec08e325f03f7
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-09 18:52:07 +0500

    Remove un-used prop

--- src/components/BestSellerSection.jsx
+++ src/components/BestSellerSection.jsx
@@ -9,11 +9,7 @@ export const BestSellerSection = ({
 	return (
 		<div className="bg-gray-300 mx-auto py-10 text-center">
 			<h2 className="text-2xl font-semibold">Bestsellers</h2>
-			<BestSellerItems
-				items={data}
-				cartItemsList={cartItemsList}
-				setCartItemsList={setCartItemsList}
-			/>
+			<BestSellerItems items={data} setCartItemsList={setCartItemsList} />
 		</div>
 	);
 };
