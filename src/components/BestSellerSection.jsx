3a98cee01d4cc882707396ec18d4b1921039df66
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-09 18:10:48 +0500

    Remove data fetching logic from BestSellerSection

--- src/components/BestSellerSection.jsx
+++ src/components/BestSellerSection.jsx
@@ -1,22 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { BestSellerItems } from "./BestSellerItems";
 
-export const BestSellerSection = ({ cartItemsList, setCartItemsList }) => {
-	const [items, setItems] = useState([]);
-	useEffect(() => {
-		const getItems = async () => {
-			const response = await fetch("https://fakestoreapi.com/products?limit=4");
-			const items = await response.json();
-			setItems([].concat(items));
-		};
-		getItems();
-	}, []);
-
+export const BestSellerSection = ({
+	data,
+	cartItemsList,
+	setCartItemsList,
+}) => {
 	return (
 		<div className="bg-gray-300 mx-auto py-10 text-center">
 			<h2 className="text-2xl font-semibold">Bestsellers</h2>
 			<BestSellerItems
-				items={items}
+				items={data}
 				cartItemsList={cartItemsList}
 				setCartItemsList={setCartItemsList}
 			/>
