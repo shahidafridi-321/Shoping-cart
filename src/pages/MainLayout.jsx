fccf19374df41cd5ce351ec20ec62924283df472
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-10 10:57:28 +0500

    Add another useEffect for fetching data for shopping page

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -6,6 +6,8 @@ import { Outlet } from "react-router-dom";
 export const MainLayout = () => {
 	const [cartItemsList, setCartItemsList] = useState([]);
 	const [items, setItems] = useState([]);
+	const [products, setProducts] = useState([]);
+
 	useEffect(() => {
 		const getItems = async () => {
 			const response = await fetch("https://fakestoreapi.com/products?limit=8");
@@ -14,13 +16,25 @@ export const MainLayout = () => {
 		};
 		getItems();
 	}, []);
+
+	useEffect(() => {
+		const getAllProducts = async () => {
+			const response = await fetch("https://fakestoreapi.com/products");
+			const products = await response.json();
+			setProducts(products);
+		};
+		getAllProducts();
+	}, []);
+
 	return (
 		<div className="flex flex-col min-h-screen">
 			<header className="sticky top-0 left-0 z-10">
 				<Navbar numberOfItemsInCart={cartItemsList.length} />
 			</header>
 			<main className="flex-grow bg-indigo-300">
-				<Outlet context={{ items, cartItemsList, setCartItemsList }} />
+				<Outlet
+					context={{ items, cartItemsList, setCartItemsList, products }}
+				/>
 			</main>
 			<footer className="w-full bg-dark py-5">
 				<Footer />
