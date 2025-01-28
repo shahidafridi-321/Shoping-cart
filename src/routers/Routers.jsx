e4d6075f1c707ad9a92901ec87a0c7ecae20cbce
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-28 15:16:28 +0500

    pass loader functions to loader prop

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
-import { MainLayout } from "../pages/MainLayout";
+import { getItemsLoader, MainLayout } from "../pages/MainLayout";
 import { Home } from "../components/Home";
-import { Shopping } from "../components/Shopping";
+import { getAllProductsLoader, Shopping } from "../components/Shopping";
 import { Cart } from "../components/Cart";
 
 export const routers = createBrowserRouter([
 	{
 		path: "/",
 		element: <MainLayout />,
+		loader: getItemsLoader,
 		children: [
 			{
 				index: true,
@@ -16,6 +17,7 @@ export const routers = createBrowserRouter([
 			{
 				path: "/shop",
 				element: <Shopping />,
+				loader: getAllProductsLoader,
 			},
 			{
 				path: "/cart",
