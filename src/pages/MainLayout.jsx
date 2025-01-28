66637fbb37c03177050beecd6cd232bd8762e3e6
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-28 15:15:54 +0500

    Add a laoder function to fetch data based on routes instead of using useEffect hook

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,30 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData } from "react-router-dom";
 
 export const MainLayout = () => {
 	const [cartItemsList, setCartItemsList] = useState([]);
-	const [items, setItems] = useState([]);
-	const [products, setProducts] = useState([]);
-
-	useEffect(() => {
-		const getItems = async () => {
-			const response = await fetch("https://fakestoreapi.com/products?limit=8");
-			const items = await response.json();
-			setItems(items);
-		};
-		getItems();
-	}, []);
-
-	useEffect(() => {
-		const getAllProducts = async () => {
-			const response = await fetch("https://fakestoreapi.com/products");
-			const products = await response.json();
-			setProducts(products);
-		};
-		getAllProducts();
-	}, []);
+	const items = useLoaderData();
 
 	return (
 		<div className="flex flex-col min-h-screen">
@@ -32,13 +13,16 @@ export const MainLayout = () => {
 				<Navbar numberOfItemsInCart={cartItemsList.length} />
 			</header>
 			<main className="flex-grow bg-indigo-300">
-				<Outlet
-					context={{ items, cartItemsList, setCartItemsList, products }}
-				/>
+				<Outlet context={{ items, cartItemsList, setCartItemsList }} />
 			</main>
 			<footer className="w-full bg-dark py-5">
 				<Footer />
 			</footer>
 		</div>
 	);
+};
+
+export const getItemsLoader = async () => {
+	const items = await fetch("https://fakestoreapi.com/products?limit=8");
+	return items.json();
 };
